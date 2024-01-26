21c2bebe16926ca3087ccdcb5822c193790b4a40
anasyakubu <yakubuanas04@gmail.com>
2024-01-26 06:24:01 +0100

    Structured Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'FocGPT',
-  description: 'Generated FocGPT'
-}
+  title: "FocGPT",
+  description: "Generated FocGPT",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }

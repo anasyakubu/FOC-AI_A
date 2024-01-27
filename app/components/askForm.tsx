b9c66193a07926513f7ec44768809ef0f21678d0
anasyakubu <yakubuanas04@gmail.com>
2024-01-27 05:22:35 +0100

    Repairs

--- app/components/askForm.tsx
+++ app/components/askForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState } from "react";
-import { CalendarDaysIcon, HandRaisedIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Logo from "../../public/logo.png";
 import { SiMinutemailer } from "react-icons/si";
@@ -55,7 +54,7 @@ const AskForm = () => {
               <p className="mt-4 text-lg leading-8 text-gray-300 text-left">
                 Tell me what`s on your mind, or pick a suggestion.
               </p>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="mt-6 flex max-w-md gap-x-4">
                   {/* <label htmlFor="email-address" className="sr-only">
                   Email address
@@ -64,7 +63,6 @@ const AskForm = () => {
                     type="text"
                     value={question}
                     onChange={(e) => setQuestion(e.target.value)}
-                    required
                     className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                     placeholder="Enter a Prompt Here"
                   />
